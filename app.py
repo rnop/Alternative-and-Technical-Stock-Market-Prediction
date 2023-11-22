f096726d121312a11c2caeaf7c4f5b7ca7c60a86
Ronnie Nop <nopronnie@gmail.com>
2023-11-22 09:50:16 -0500

    openai gpt update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
+
+import pandas as pd
+import numpy as np
+
+import utils
 import yfinance as yf
 from datetime import datetime, timedelta
 import pandas_ta as ta
-import pandas as pd
-import numpy as np
 
 import plotly
 import plotly.graph_objects as go
@@ -12,6 +15,7 @@ import json
 
 from joblib import load
 
+import openai
 
 app = Flask(__name__)
 
@@ -27,48 +31,19 @@ def index():
         user_input = request.form['ticker']
         
         # Attempt to fetch data for the user-provided ticker
-        df = yf.download(user_input, start=start_date.strftime('%Y-%m-%d'))
-        
-        # Check if the data is valid, if not, use the default ticker
-        if df.empty:
-            df = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'))
-        else:
-            ticker = user_input  # Update ticker to user input if valid
+        df = get_stock_data(user_input, start_date)
 
-        df['SMA_50'] = ta.sma(df['Close'], length=50)
-        df['SMA_200'] = ta.sma(df['Close'], length=200)
-
-        df['EMA_12'] = ta.ema(df['Close'], length=12)
-        df['EMA_26'] = ta.ema(df['Close'], length=26)
-
-        df['RSI'] = ta.rsi(df['Close'], length=14)
-
-        macd = ta.macd(df['Close'], fast=12, slow=26, signal=9)
-        df = df.join(macd)
-
-        bollinger = ta.bbands(df['Close'], length=20, std=2)
-        df = df.join(bollinger)
-        df.dropna(inplace=True)
-        
-        # Train features
-        df['SMA50_PCTCHG'] = ((df['SMA_50'] - df['Close']) / df['Close']) * 100
-        df['SMA200_PCTCHG'] = ((df['SMA_200'] - df['Close']) / df['Close']) * 100
-        df['SMA_BULLISH'] = (df['SMA_50'] > df['SMA_200']).astype(int)
-        
-        df['EMA12_PCTCHG'] = ((df['EMA_12'] - df['Close']) / df['Close']) * 100
-        df['EMA26_PCTCHG'] = ((df['EMA_26'] - df['Close']) / df['Close']) * 100
-        df['EMA_BULLISH'] = (df['EMA_12'] > df['EMA_26']).astype(int)
-        
-        df['LOWERBBAND_PCTCHG'] = ((df['BBL_20_2.0'] - df['Close']) / df['Close']) * 100
-        df['MIDBBAND_PCTCHG'] = ((df['BBM_20_2.0'] - df['Close']) / df['Close']) * 100
-        df['UPPERBBAND_PCTCHG'] = ((df['BBU_20_2.0'] - df['Close']) / df['Close']) * 100
-
-        
+        # Sort and get start, end dates
         df_sorted = df.sort_index().tail(90)
-
-        # Get starting and end dates
         df_start = df_sorted.index[0].strftime('%Y-%m-%d')
         df_end = df_sorted.index[-1].strftime('%Y-%m-%d')
+    
+        # Add trading signals
+        df['BUY_PREDICTION'] = generate_bull_signal(df)
+        df['SELL_PREDICTION'] = generate_bear_signal(df)
+
+        # Last Sort df
+        df_sorted = df.sort_index().tail(90)
 
         # Plotly chart code
         fig = make_subplots(rows=3, cols=1, shared_xaxes=True, 
@@ -88,30 +63,7 @@ def index():
         
         fig.add_trace(candlestick, row=1, col=1)
 
-        # Add trading signals
-        # Generate "random" machine learning model predictions
-        bull_model = load('bull_rf_model_threshold81.joblib')
-        bear_model = load('bear_rf_model_threshold52.joblib')
-
-        train_features = ['SMA50_PCTCHG','SMA200_PCTCHG', 'SMA_BULLISH','EMA12_PCTCHG', 'EMA26_PCTCHG', 'EMA_BULLISH', 'LOWERBBAND_PCTCHG', 'MIDBBAND_PCTCHG', 'UPPERBBAND_PCTCHG', 'RSI']
-        # bull predictions
-        prob_predictions = bull_model.predict_proba(df[train_features])
-        prob_class_1 = prob_predictions[:, 1]
-        threshold = 0.7
-        threshold_predictions = np.where(prob_class_1 > threshold, 1, 0)
-        df['BUY_PREDICTION'] = threshold_predictions
-
-        # bear predictions
-        prob_predictions = bear_model.predict_proba(df[train_features])
-        prob_class_1 = prob_predictions[:, 1]
-        threshold = 0.52
-        threshold_predictions = np.where(prob_class_1 > threshold, 1, 0)
-        df['SELL_PREDICTION'] = threshold_predictions
-
-        # Sort df
-        df_sorted = df.sort_index().tail(90)
-
-        # Add trading signals
+        # Boolean mask for trading signals
         buy_signals = df_sorted[df_sorted['BUY_PREDICTION'] == 1]
         sell_signals = df_sorted[df_sorted['SELL_PREDICTION'] == 1]
 
@@ -156,7 +108,6 @@ def index():
         fig.add_hline(y=70, line_dash="dash", line_color="red", row=3, col=1)
         fig.add_hline(y=30, line_dash="dash", line_color="green", row=3, col=1)
 
-
         fig.update_layout(
             width=1400, 
             height=800,
@@ -173,6 +124,15 @@ def index():
             yaxis3_title="RSI"     # Set y-axis title for RSI subplot
         )   
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # Connect to OpenAI
+        ## --------------- ommitted --------------- ##
+
+        # Design system and user prompts
+        ## --------------- ommitted --------------- ##
+        
+        # Generate response from GPT model
+        ## --------------- ommitted --------------- ##
 
     return render_template('index.html', graphJSON=graphJSON, ticker=ticker)
 
